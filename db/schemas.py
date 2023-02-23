c88532a88f71bad9a5775f78021df394df5ce37b
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2023-02-23 23:20:11 +0530

    adds endpoint to provide api-keys

--- db/schemas.py
+++ db/schemas.py
@@ -2,10 +2,15 @@ from pydantic import BaseModel, Field
 
 
 class User(BaseModel):
-    user_name: str = Field(min_length=1, max_length=20)
-    email: str = Field(min_length=1, max_length=50)
-    is_active: bool
-    api_key: str
+    email: str = Field(min_length=7, max_length=30, title='email ID')
+
+    # Default config override
+    class Config:
+        schema_extra = {
+            'example': {
+                "email": "xyz@gmail.com",
+            }
+        }
 
 
 class Todo(BaseModel):
--- todoapp.py
+++ todoapp.py
@@ -1,13 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+import secrets
 from db import models
-from db.database import engine
-
+from db.schemas import User, Todo
+from sqlalchemy.orm import Session
+from db.database import engine, SessionLocal
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
 
-@app.get('/')
-async def hello():
-    return {'message': 'Hello FastAPI'}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# @app.get('/')
+# async def hello():
+#     return {'message': 'Hello FastAPI'}
+
+
+@app.post('/api_key')
+async def get_api_key(user: User, db: Session = Depends(get_db)):
+
+    existing_user = db.query(models.UserEntity).filter(models.UserEntity.email == user.email).first()
+    if existing_user:
+        return {'api_key': db.query(models.UserEntity).filter(models.UserEntity.email == user.email).first().api_key}
+
+    new_user = models.UserEntity()
+    new_user.email = user.email
+    new_user.api_key = secrets.token_hex(64)
+
+    db.add(new_user)
+    db.commit()
+    return {'api_key': new_user.api_key}
