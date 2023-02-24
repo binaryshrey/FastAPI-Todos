1727d8b6020a4526478bacf42f465b0e3826f8b7
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2023-02-24 16:09:51 +0530

    adds deployment scripts

--- main.py
+++ main.py
@@ -100,7 +100,3 @@ async def get_api_key(user: User, db: Session = Depends(get_db)):
     db.add(new_user)
     db.commit()
     return {'api_key': new_user.api_key}
-
-
-if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=80, log_level='info')
